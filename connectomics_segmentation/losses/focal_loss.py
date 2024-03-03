83b028dd7b7654d5918e35613094f14bb6a80a3a
Oleh Shkalikov <Oleh.Shkalikov@mailbox.tu-dresden.de>
2024-03-03 08:56:43 +0100

    Fix FocalLoss for batch size greater than 1

--- connectomics_segmentation/losses/focal_loss.py
+++ connectomics_segmentation/losses/focal_loss.py
@@ -19,10 +19,13 @@ class FocalLoss(nn.Module):
         self.ignore_idx = ignore_index
         self.label_smoothing = label_smoothing
 
-    def __call__(self, pred, target):
-        mask = target != self.ignore_idx
-        masked_pred = pred[:, :, mask[0]]
-        masked_target = target[mask].unsqueeze(dim=0)
+    def __call__(self, pred: torch.Tensor, target: torch.Tensor):
+        flatten_pred = pred.permute(0, 2, 3, 4, 1).flatten(end_dim=3)
+        flatten_target = target.flatten()
+
+        mask = flatten_target != self.ignore_idx
+        masked_pred = flatten_pred[mask]
+        masked_target = flatten_target[mask]
 
         p = F.softmax(masked_pred, dim=1).gather(1, masked_target.unsqueeze(dim=1))
         mult = (1 - p) ** self.gamma
